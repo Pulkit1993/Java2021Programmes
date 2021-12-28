c63974a611c8e3e10464175635f7d9dd4fb9cbde
pulkit6404 <pulkitrydes@gmail.com>
2021-12-28 18:40:27 +0530

    added/updated/deletd files

--- src/Arrays/Array1.java
+++ src/Arrays/Array1.java
@@ -6,7 +6,7 @@ public class Array1 {
 	public static void main(String[] args) 
 	{
 		
-		int a[] = {20,30,8,46,52,9};
+		int a[] = {20,30,8,46,52,9,54};
 		int key=52;
 		for(int i=0; i<a.length; i++)
 		{
